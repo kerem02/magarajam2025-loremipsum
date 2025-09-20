3c3cd37aeb82f567f397b2fce319e6e81ab13ff4
Ege Uzun <38254705+eegeuzunn@users.noreply.github.com>
2025-09-20 19:10:06 +0300

    web spawner

---
+++ Assets/Scripts/WebSpawner.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebSpawner : MonoBehaviour
+{
+    public GameObject web;
+    public bool isWebCreated = false;
+    public WebController webController;
+    private Renderer renderer;
+    private Collider col;
+
+    public float spawnWebDuration = 2f;
+    private float holdCounter = 0f;
+
+    private bool inTrigger = false;
+
+    private void Awake()
+    {
+        webController = web.GetComponent<WebController>();
+        renderer = web.GetComponent<Renderer>();
+        col = web.GetComponent<Collider>();
+        destroyWeb();
+    }
+
+    private void Update()
+    {
+        if (inTrigger && !isWebCreated)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                web.SetActive(true);
+                holdCounter += Time.deltaTime;
+
+                Color c = renderer.material.color;
+                c.a = Mathf.Clamp01(holdCounter / spawnWebDuration);
+                renderer.material.color = c;
+
+                if (holdCounter >= spawnWebDuration)
+                {
+                    col.enabled = true;
+                    isWebCreated = true;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                holdCounter = 0f;
+                destroyWeb();
+            }
+        }
+        else
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+        holdCounter = 0f;
+    }
+
+    public void destroyWeb()
+    {
+        Color color = renderer.material.color;
+        color.a = 0f;
+        renderer.material.color = color;
+        web.SetActive(false);
+        isWebCreated = false;
+        col.enabled = false;
+        webController.destroyAllFlies();
+    }
+}
